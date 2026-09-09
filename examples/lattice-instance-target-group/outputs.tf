676205c6b4793905a9f86a1c8aa103a408102b0a
Byungjin Park <posquit0.bj@gmail.com>
2026-09-09 10:23:43 +0900

    refactor!: remove VPC Lattice modules migrated to terraform-aws-vpc-lattice
    BREAKING CHANGE: All `lattice-*` modules and examples are removed from this
    repository. They have been migrated to tedilabs/terraform-aws-vpc-lattice.

--- examples/lattice-instance-target-group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "target_group" {
-  description = "The Instance target group of VPC Lattice."
-  value       = module.target_group
-}
-
-output "instances" {
-  description = "The EC2 instances."
-  value       = module.instance
-}
--- examples/lattice-service-network-simple/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service_network" {
-  description = "The service network of VPC Lattice."
-  value       = module.service_network
-}
